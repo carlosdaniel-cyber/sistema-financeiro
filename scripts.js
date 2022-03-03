fb47a32960c9df556f9400b745f985f770be47f7
Carlos Daniel <carlosdanielbp101@gmail.com>
2022-03-03 18:26:42 -0300

    adição das operações financeiras ao html por meio do JS

--- scripts.js
+++ scripts.js
@@ -45,16 +45,35 @@ const Transaction = {
 }
 
 const DOM = {
-    innerHTMLTransaction() {
+    transactionsContainer: document.querySelector('#data-table tbody'),
+    addTransaction(transaction, index) {
+        const tr = document.createElement('tr');
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+        DOM.transactionsContainer.appendChild(tr)
+    },
+    innerHTMLTransaction(transaction) {
+        const CSSclass = transaction.amount > 0 ? "income" : "expense"
+
+        const amount = Utils.formatCurrency(transaction.amount)
+
         const html = `
-        <tr>
-            <td class="description">Luz</td>
-            <td class="expense">- R$500,00</td>
-            <td class="date">23/01/2021</td>
+            <td class="description">${transaction.description}</td>
+            <td class="${CSSclass}">${transaction.amount}</td>
+            <td class="date">${transaction.date}</td>
             <td>
                 <img src="./assets/minus.svg" alt="Remover transações">
             </td>
-        </tr>
         `
+        return html
     }
 }
+
+const Utils = {
+    formatCurrency(value) {
+        const signal = Number(value) < 0 ? "-" : ""
+    }
+}
+
+transactions.forEach(transaction => {
+    DOM.addTransaction(transaction)
+})
