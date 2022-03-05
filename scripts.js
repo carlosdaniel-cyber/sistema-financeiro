5cd2afe274eef4ea9b6ae8651e6d6f84e7d412b9
Carlos Daniel <carlosdanielbp101@gmail.com>
2022-03-05 12:39:57 -0300

    formatação e display dos valores e das operações e cálculo de entradas, saídas e total

--- scripts.js
+++ scripts.js
@@ -29,28 +29,58 @@ const transactions = [
         description: 'Internet',
         amount: -20000,
         date: '23/01/2021'
+    },
+    {
+        id: 4,
+        description: 'App',
+        amount: 42069,
+        date: '04/03/2021'
     }
 ]
 
 const Transaction = {
+    all: transactions,
+
+    add(transaction) {
+        Transaction.all.push(transaction)
+
+        App.reload()
+    },
+
     incomes () {
-        // somar as esntradas
+        let income = 0
+        Transaction.all.forEach(transaction => {
+            if (transaction.amount > 0) {
+                income += transaction.amount
+            }
+        })
+        return income
     },
+
     expenses () {
-        // somar as saídas
+        let expense = 0
+        Transaction.all.forEach(transaction => {
+            if (transaction.amount < 0) {
+                expense += transaction.amount
+            }
+        })
+        return expense
     },
+
     total () {
-        // entradas -saídas
+        return Transaction.incomes() + Transaction.expenses()
     }
 }
 
 const DOM = {
     transactionsContainer: document.querySelector('#data-table tbody'),
+
     addTransaction(transaction, index) {
         const tr = document.createElement('tr');
         tr.innerHTML = DOM.innerHTMLTransaction(transaction)
         DOM.transactionsContainer.appendChild(tr)
     },
+
     innerHTMLTransaction(transaction) {
         const CSSclass = transaction.amount > 0 ? "income" : "expense"
 
@@ -58,22 +88,63 @@ const DOM = {
 
         const html = `
             <td class="description">${transaction.description}</td>
-            <td class="${CSSclass}">${transaction.amount}</td>
+            <td class="${CSSclass}">${amount}</td>
             <td class="date">${transaction.date}</td>
             <td>
                 <img src="./assets/minus.svg" alt="Remover transações">
             </td>
         `
         return html
+    },
+
+    updateBalance() {
+        document.getElementById('incomeDisplay').innerHTML = Utils.formatCurrency(Transaction.incomes())
+        document.getElementById('expenseDisplay').innerHTML = Utils.formatCurrency(Transaction.expenses())
+        document.getElementById('totalDisplay').innerHTML = Utils.formatCurrency(Transaction.total())
+    }, 
+
+    clearTransactions() {
+        DOM.transactionsContainer.innerHTML = ""
     }
 }
 
 const Utils = {
     formatCurrency(value) {
         const signal = Number(value) < 0 ? "-" : ""
+
+        value = String(value).replace(/\D/g, "") // RegEx
+        
+        value = Number(value) / 100
+
+        value = value.toLocaleString("pt-br", {
+            style: "currency",
+            currency: "brl"
+        })
+
+        return signal + value
     }
 }
 
-transactions.forEach(transaction => {
-    DOM.addTransaction(transaction)
+const App = {
+    init() {
+        transactions.forEach(transaction => {
+            DOM.addTransaction(transaction)
+        })
+
+        DOM.updateBalance()
+    },
+
+    reload() {
+        DOM.clearTransactions()
+        App.init()
+    }
+}
+
+App.init()
+
+Transaction.add({
+    id: 5,
+    description: 'Mercado',
+    amount: 100000,
+    date: '05/03/2022'
 })
